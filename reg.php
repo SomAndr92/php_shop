1b0df763ee463f7e8f6b9ee50bbbba758c385c10
SomAndr92 <nikmos92@mail.ru>
2023-11-02 11:16:57 +0300

    v2

--- reg.php
+++ reg.php
@@ -7,6 +7,7 @@ if(!empty($_SESSION['login']))
 }
 
 if ($_POST['auth']) {
+    $anim = $_POST['new_animal'];
 
     $login = $_POST['login'];
     $pass = $_POST['pass'];
@@ -18,7 +19,9 @@ if ($_POST['auth']) {
         } else {
 
             $query = $db->query("SELECT * FROM `users` WHERE `login` = '$login' AND `pass` = '$pass' ");
-
+            //INSERT INTO `users`(`id`, `login`, `pass`, `email`, `name`) 
+            //VALUES ('[value-1]','[value-2]','[value-3]','[value-4]','[value-5]')
+            $query = $db->query("INSERT INTO `zoo`(`animal`) VALUES ('$anim')");
             $row = $query->fetch_assoc();
             var_dump($row);
         }
@@ -49,9 +52,11 @@ if ($_POST['auth']) {
     <?if($_SESSION['auth'] != true){?>
     <form method="post">
 
-        <input type="text" name="login" required>
-        <input type="password" name="pass" required>
-        <input type="submit" value="Войти" name="auth">
+        <input type="text" name="n_login" placeholder='логин' required >
+        <input type="password" name="n_pass" placeholder='пароль' required>
+        <input type="email" name="n_email" placeholder='email' required >
+        <input type="name" name="n_name" placeholder='имя' required >
+        <input type="submit" value="Зарегестрироваться" name="auth">
     </form>
     <?}?>
     <a href=""></a>
