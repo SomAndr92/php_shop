2f84382550ba0a401592afa601c406418eab5f38
SomAndr92 <nikmos92@mail.ru>
2023-11-02 11:46:45 +0300

    v3

--- reg.php
+++ reg.php
@@ -7,32 +7,29 @@ if(!empty($_SESSION['login']))
 }
 
 if ($_POST['auth']) {
-    $anim = $_POST['new_animal'];
 
-    $login = $_POST['login'];
-    $pass = $_POST['pass'];
+    $new_login = $_POST['n_login'];
+    $new_pass = $_POST['n_pass'];
+    $new_email = $_POST['n_email'];
+    $new_name = $_POST['n_name'];
 
-    if (!empty($login) and !empty($pass)) {
+    if (!empty($new_login) and !empty($new_pass) and !empty($new_email) and !empty($new_name)) {
         $db = @new mysqli('localhost', 'root', '', 'shop');
         if ($db->connection_errno) {
             echo "error: " . $db->connection_errno;
         } else {
+            echo ("aaa");
 
-            $query = $db->query("SELECT * FROM `users` WHERE `login` = '$login' AND `pass` = '$pass' ");
-            //INSERT INTO `users`(`id`, `login`, `pass`, `email`, `name`) 
+            //$query = $db->query("SELECT * FROM `users` WHERE `login` = '$login' AND `pass` = '$pass' ");
+            //INSERT INTO  
             //VALUES ('[value-1]','[value-2]','[value-3]','[value-4]','[value-5]')
-            $query = $db->query("INSERT INTO `zoo`(`animal`) VALUES ('$anim')");
-            $row = $query->fetch_assoc();
-            var_dump($row);
+            $query = $db->query("INSERT INTO `users`(`login`, `pass`, `email`, `name`) 
+            VALUES ('$new_login', '$new_pass', '$new_email', '$new_name')");          
         }
     }
-
 }
 
-
 ?>
-
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -40,7 +37,7 @@ if ($_POST['auth']) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Регистрация</title>
     <link rel="stylesheet" href="style.css">
 </head>
 
@@ -56,10 +53,9 @@ if ($_POST['auth']) {
         <input type="password" name="n_pass" placeholder='пароль' required>
         <input type="email" name="n_email" placeholder='email' required >
         <input type="name" name="n_name" placeholder='имя' required >
-        <input type="submit" value="Зарегестрироваться" name="auth">
+        <input type="submit" value="Зарегистрироваться" name="auth">
     </form>
     <?}?>
-    <a href=""></a>
 
 </body>
 
